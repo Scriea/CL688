1482f1992570e00b41158988e72709e8d2b794c2
Scriea <ashumulchandani8@gmail.com>
2023-09-06 22:32:27 +0530

    Update code files

--- Project/code/heatmap.py
+++ Project/code/heatmap.py
@@ -13,16 +13,14 @@ data = pd.read_csv(PATH_DATA)
 data = data.iloc[:, 5:15]
 print(data.head())
 
-# Step 3: Compute the covariance matrix
-cov_matrix = data.corr()  # Set rowvar=False to treat columns as variables
+cov_matrix = data.corr()  
 
-# # Step 4: Create a heatmap using seaborn
-plt.figure(figsize=(18, 15))  # Adjust the figure size
-sns.set(font_scale=1)
+plt.figure(figsize=(18, 15))  
+sns.set(font_scale=2)
 heatmap = sns.heatmap(cov_matrix, annot=True, fmt=".2f", cmap="coolwarm", xticklabels=data.columns, yticklabels=data.columns, cbar=False)
 heatmap.set_xticklabels(heatmap.get_xticklabels(), rotation=45, horizontalalignment='right')
 plt.title("Covariance Heatmap (First 10 Features)")
-plt.savefig('covariance_heatmap.png', bbox_inches='tight')  # 'bbox_inches' prevents clipping of labels
+plt.savefig('covariance_heatmap.png', bbox_inches='tight')
 plt.show()
 
 
